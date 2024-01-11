9a8e40039bb63b7c111b386c5f900656771ff803
mkrol11 <148458025+mkrol11@users.noreply.github.com>
2024-01-11 20:34:16 +0100

    Update server.R

--- server.R
+++ server.R
@@ -2,9 +2,11 @@ library(shiny)
 source("./servers/gym.R")
 source("./servers/moodServer.R")
 source("./servers/sleepServer.R")
+source("./servers/foodServer.R")
 
 server <- function(input, output, session) {
   gymLogic(input, output, session)
   moodLogic(input, output, session)
   sleepLogic(input, output, session)
+  foodLogic(input, output, session)
 }
