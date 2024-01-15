aafec7c054ae5c8d03bf837413f51cd3b0dabe0f
Mateusz Król <148458025+mkrol11@users.noreply.github.com>
2024-01-15 18:38:12 +0100

    Update food.R

--- ui/food.R
+++ ui/food.R
@@ -1,17 +1,40 @@
-foodUI <- fluidPage(
-  titlePanel("Przyjmowane mikroelementy"),
+generate_nutrition_ui <- function() {
 
-    
-  mainPanel(
-    selectInput("nawykresie",
-                "Które parametry chcesz zobaczyć?",
-                list("Węglowodany", "Białka", "Cukry"),
-                selected = list("Węglowodany", "Białka", "Cukry"),
-                multiple = TRUE),
+  foodUI <- fluidPage(
+    titlePanel("Analysis of diet and nutrition"),
+  
+    sidebarLayout(  
 
-    plotlyOutput("foodPlot")
+      mainPanel(
+        box(
+          title = "Macroelement intake",
+          status = "warning",
+          width = 12,
+          solidHeader = TRUE,
+          collapsible = FALSE,
+          plotlyOutput("foodPlot")
+        )
+        ),
+      sidebarPanel(
+  
+        selectInput("plotMeanFood",
+                    "Show means of which parameters?",
+                    list("Fats", "Carbohydrates", "Protein", "Sugar"),
+                    selected = list("Fats", "Protein"),
+                    multiple = TRUE),
+        
+        box(
+          title = "Panel na jakies tam gadanie",
+          status = "warning",
+          width = 12,
+          solidHeader = TRUE,
+          collapsible = FALSE,
+          p("Jakies tam gadanie, pozniej sprobuje dorobic jeszcze jeden wykres nizej", 
+            style = "font-size: 20px;"),
+        )
+      )
     )
   )
-
-
-return(foodUI)
+  
+  return(foodUI)
+}
