25a646e7e72db8221992f80fb992794f0ff2473d
Mateusz Król <148458025+mkrol11@users.noreply.github.com>
2024-01-19 00:14:24 +0100

    Nutrition - implemented dark style

--- src/food/wykres1.R
+++ src/food/wykres1.R
@@ -19,52 +19,59 @@ foodPlot <- function(datasource, showmode){
     fig <- plot_ly(daily_calories)
   }
     
-  fig <- fig %>% layout(title = 'Daily intake of selected macroelements', 
-                      xaxis = list(title = 'Date'), 
-                      yaxis = list(title = 'Daily intake (grams)'),
-                      paper_bgcolor = 'gray60',
-                      plot_bgcolor = 'gray80',
-                      hovermode = 'x unified') %>% 
+  fig <- fig %>% layout(title = list(text = 'Daily intake of selected macroelements', 
+                                     font = list(color = 'white')),
+                      xaxis = list(title = list(text = 'Date', 
+                                                font = list(color = 'white')),
+                                   tickfont = list(color = 'white')), 
+                      yaxis = list(title = list(text = 'Daily intake (grams)',
+                                                font = list(color = 'white')),
+                                   tickfont = list(color = 'white')),
+                      legend = list(font = list(color = 'white')),
+                      plot_bgcolor = 'rgba(0, 0, 0, 0)',
+                      paper_bgcolor = 'rgba(0, 0, 0, 0)',
+                      hovermode = 'x unified',
+                      hoverlabel = list(bgcolor = 'rgba(20, 20, 20, 120)')) %>% 
     add_trace(x = ~Data, y = ~mean(total_fats),
                              type = 'scatter',
                              name = 'Fat mean', 
                              mode = 'lines',
-                             line = list(color = 'gold')) %>% 
+                             line = list(color = 'rgba(150, 110, 5, 255)')) %>% 
     add_trace(x = ~Data, y = ~mean(total_carbs), 
                              type = 'scatter',
                              name = 'Carb. mean', 
                              mode = 'lines',
-                             line = list(color = 'darkturquoise')) %>% 
+                             line = list(color = 'rgba(25, 10, 220, 255')) %>% 
     add_trace(x = ~Data, y = ~mean(total_protein), 
                              type = 'scatter',
                              name = 'Protein mean', 
                              mode = 'lines',
-                             line = list(color = 'lightcoral')) %>% 
+                             line = list(color = 'rgba(170, 20, 5, 255)')) %>% 
     add_trace(x = ~Data, y = ~mean(total_sugar),
                              type = 'scatter',
                              name = 'Sugar mean', 
                              mode = 'lines',
-                             line = list(color = 'lightslategrey'))  %>%
+                             line = list(color = 'dimgrey'))  %>%
     add_trace(x = ~Data, y = ~total_fats, 
               name = 'Fats', 
               mode = 'lines+markers',
-              line = list(color = 'orange', shape = 'spline'),
-              marker = list(color = 'orange')) %>% 
+              line = list(color = 'gold', shape = 'spline'),
+              marker = list(color = 'gold')) %>% 
     add_trace(x = ~Data, y = ~total_carbs, 
               name = 'Carbohydrates', 
               mode = 'lines+markers', 
-              line = list(color = 'blue', shape = 'spline'),
-              marker = list(color = 'blue')) %>%
+              line = list(color = 'darkturquoise', shape = 'spline'),
+              marker = list(color = 'darkturquoise')) %>%
     add_trace(x = ~Data, y = ~total_protein, 
               name = 'Protein', 
               mode = 'lines+markers', 
-              line = list(color = 'red', shape = 'spline'),
-              marker = list(color = 'red')) %>%
+              line = list(color = 'rgba(255, 30, 50, 255)', shape = 'spline'),
+              marker = list(color = 'rgba(255, 30, 50, 255)')) %>%
     add_trace(x = ~Data, y = ~total_sugar, 
               name = 'Sugar', 
               mode = 'lines+markers',
-              line = list(color = 'black', shape = 'spline'),
-              marker = list(color = 'black'))
+              line = list(color = 'azure', shape = 'spline'),
+              marker = list(color = 'azure'))
 
   return(fig)
 }
@@ -92,6 +99,8 @@ caloriePlot <- function(datasource){
   )
   
   fig <- fig %>% layout(
+    plot_bgcolor = 'rgba(0, 0, 0, 0)',
+    paper_bgcolor = 'rgba(0, 0, 0, 0)',
     polar = list(
       radialaxis = list(visible = TRUE, 
                         range = c(0, 71),
@@ -107,11 +116,9 @@ caloriePlot <- function(datasource){
                          ticktext = seq.Date(as.Date(min(daily_calories$numdate)), as.Date(max(daily_calories$numdate)), by = 1)
     ),
     showlegend = FALSE,
-    paper_bgcolor = 'gray60',
-    plot_bgcolor = 'gray80',
-    title = 'Sum of kcal consumed on each day'
+    title = list(text = 'Sum of kcal consumed on each day', 
+                 font = list(color = 'white'))
   )
   
   return(fig)
 }
-
