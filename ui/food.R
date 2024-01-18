b4c3e32d874579fc32da24d901a6215d457ec4b6
Mateusz Król <148458025+mkrol11@users.noreply.github.com>
2024-01-18 22:30:46 +0100

    Update food.R

--- ui/food.R
+++ ui/food.R
@@ -6,6 +6,15 @@ generate_nutrition_ui <- function() {
     sidebarLayout(  
 
       mainPanel(
+
+        box(
+          title = "Daily calorie intake",
+          status = "warning",
+          width = 10,
+          solidHeader = TRUE,
+          collapsible = FALSE,
+          plotlyOutput("caloriePlot")
+        ),
         box(
           title = "Macroelement intake",
           status = "warning",
@@ -13,14 +22,6 @@ generate_nutrition_ui <- function() {
           solidHeader = TRUE,
           collapsible = FALSE,
           plotlyOutput("foodPlot")
-        ),
-        box(
-          title = "Daily calorie intake",
-          status = "warning",
-          width = 12,
-          solidHeader = TRUE,
-          collapsible = FALSE,
-          plotlyOutput("caloriePlot")
         )
         ),
       sidebarPanel(
@@ -37,8 +38,15 @@ generate_nutrition_ui <- function() {
           width = 12,
           solidHeader = TRUE,
           collapsible = FALSE,
-          p("Jakies tam gadanie, pozniej sprobuje dorobic jeszcze jeden wykres nizej", 
-            style = "font-size: 20px;"),
+          HTML(paste('<span style="font-size: 20px;">As a popular saying goes, <strong>"You are what you eat"</strong>. 
+            Indeed, what we eat and how we eat may have greater impact on our wellbeing than most people think
+             - which is why we have decided to track our daily meals as well. <br> <br>
+            
+            <strong>Polar chart on the left presents total energetic value</strong> of meals on each day. 
+            Each day is a separate slice, and amount of consumed kcal on each day is proportional to area of slice.
+            Do not be alarmed by very low values, as they were most probably caused by incomplete tracking. 
+            However, nutrition is not just about counting calories, and so the <strong>second plot shows daily intake of crucial macroelements</strong>:
+            carbohydrates, protein, as well as fats and sugar.')),
         )
       )
     )
