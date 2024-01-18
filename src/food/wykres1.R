3f2e5e064bed728175362898d5fbf6179b0ede14
Mateusz Król <148458025+mkrol11@users.noreply.github.com>
2024-01-18 23:21:32 +0100

    Update wykres1.R

--- src/food/wykres1.R
+++ src/food/wykres1.R
@@ -2,7 +2,7 @@ library(shiny)
 library(dplyr)
 library(tidyr)
 
-foodPlot <- function(datasource, params, showmode){
+foodPlot <- function(datasource, showmode){
 
   daily_calories <- read.csv(datasource) %>%
     group_by(Data) %>%
@@ -18,42 +18,33 @@ foodPlot <- function(datasource, params, showmode){
     }  else {
     fig <- plot_ly(daily_calories)
   }
-  
-  if ("Fats" %in% params == TRUE) {
-    fig <- fig %>% add_trace(x = ~Data, y = ~mean(total_fats),
-                             type = 'scatter',
-                             name = 'Fat mean', 
-                             mode = 'lines',
-                             line = list(color = 'gold'))
-  }
-  if ("Carbohydrates" %in% params == TRUE) {
-    fig <- fig %>% add_trace(x = ~Data, y = ~mean(total_carbs), 
-                             type = 'scatter',
-                             name = 'Carb. mean', 
-                             mode = 'lines',
-                             line = list(color = 'darkturquoise'))
-  }  
-  if ("Protein" %in% params == TRUE) {
-    fig <- fig %>% add_trace(x = ~Data, y = ~mean(total_protein), 
-                             type = 'scatter',
-                             name = 'Protein mean', 
-                             mode = 'lines',
-                             line = list(color = 'lightcoral'))
-  }
-  if ("Sugar" %in% params == TRUE) {
-    fig <- fig %>% add_trace(x = ~Data, y = ~mean(total_sugar),
-                             type = 'scatter',
-                             name = 'Sugar mean', 
-                             mode = 'lines',
-                             line = list(color = 'lightslategrey')) 
-  }
-  
+    
   fig <- fig %>% layout(title = 'Daily intake of selected macroelements', 
                       xaxis = list(title = 'Date'), 
                       yaxis = list(title = 'Daily intake (grams)'),
                       paper_bgcolor = 'gray60',
                       plot_bgcolor = 'gray80',
-                      hovermode = 'x unified') %>%
+                      hovermode = 'x unified') %>% 
+    add_trace(x = ~Data, y = ~mean(total_fats),
+                             type = 'scatter',
+                             name = 'Fat mean', 
+                             mode = 'lines',
+                             line = list(color = 'gold')) %>% 
+    add_trace(x = ~Data, y = ~mean(total_carbs), 
+                             type = 'scatter',
+                             name = 'Carb. mean', 
+                             mode = 'lines',
+                             line = list(color = 'darkturquoise')) %>% 
+    add_trace(x = ~Data, y = ~mean(total_protein), 
+                             type = 'scatter',
+                             name = 'Protein mean', 
+                             mode = 'lines',
+                             line = list(color = 'lightcoral')) %>% 
+    add_trace(x = ~Data, y = ~mean(total_sugar),
+                             type = 'scatter',
+                             name = 'Sugar mean', 
+                             mode = 'lines',
+                             line = list(color = 'lightslategrey'))  %>%
     add_trace(x = ~Data, y = ~total_fats, 
               name = 'Fats', 
               mode = 'lines+markers',
@@ -104,18 +95,23 @@ caloriePlot <- function(datasource){
     polar = list(
       radialaxis = list(visible = TRUE, 
                         range = c(0, 71),
-                        tickvals = list(sqrt(250), sqrt(500), sqrt(1000), sqrt(2000), sqrt(3000), sqrt(4000)),
-                        ticktext = list('250', '500', '1000', '2000', '3000', '4000')), #Now intake is relative to field of slice
+                        tickvals = list(sqrt(250), sqrt(500), sqrt(1000), 
+                                        sqrt(1500), sqrt(2000), sqrt(2500),
+                                        sqrt(3000), sqrt(4000), sqrt(5000)),
+                        ticktext = list('250', '500', '1000', '1500', '2000', 
+                                        '2500', '3000', '4000', '5000')), #Now intake is relative to field of slice
       angularaxis = list(direction = "clockwise", 
                          rotation = 270, 
                          dtick = 360/(max(daily_calories$numdate) - min(daily_calories$numdate) + 1),
-                         showticklabels = FALSE)
-                         #ticktext = seq.Date(as.Date(min(daily_calories$numdate)), as.Date(max(daily_calories$numdate)), by = 1))
+                         showticklabels = FALSE),
+                         ticktext = seq.Date(as.Date(min(daily_calories$numdate)), as.Date(max(daily_calories$numdate)), by = 1)
     ),
     showlegend = FALSE,
     paper_bgcolor = 'gray60',
-    plot_bgcolor = 'gray80'
+    plot_bgcolor = 'gray80',
+    title = 'Sum of kcal consumed on each day'
   )
   
   return(fig)
 }
+
