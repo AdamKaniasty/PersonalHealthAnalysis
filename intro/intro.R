0ecabb0c38c1e601c9baadf6ed24f72148f10b97
Mateusz Król <148458025+mkrol11@users.noreply.github.com>
2024-01-19 18:27:43 +0100

    Minor style change

--- intro/intro.R
+++ intro/intro.R
@@ -2,7 +2,7 @@ intro_columns <- function(image_path, title, description, github_link, linkedin_
   column(
     width = 12,
     img(src = image_path, class = "img-circle", align = 'center'),
-    h4(title),
+    h3(title),
     p(description, align = 'center'),
     a(icon("github"), href = github_link, target = "_blank", style = "font-size: 50px;"),
     a(icon("linkedin"), href = linkedin_link, target = "_blank", style = "font-size: 50px;")
